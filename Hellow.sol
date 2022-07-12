1b996bc14f90e8a5f476056666c2f192d4f46d8f
harshpatel100 <90831761+harshpatel100@users.noreply.github.com>
2022-07-12 14:33:23 +0530

    Create Hellow.sol

---
+++ Hellow.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.13;
+
+//check compiler version first
+//here version must be greater than or equals to 0.8.13 and less than 0.9.0
+
+// no need to match file name & Contract name
+//one file "here Hellow.sol" contains only one contract "Hellow" 
+// sometime one file contains many contract also
+
+contract Hellow{
+  
+  string public greet = "Hellow, World ! ";
+  
+}
